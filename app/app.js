86705cea5b951fc9343d6f57cdedbfd3103ad233
obalbous <oba@squid>
2014-06-23 18:03:55 +0200

    refactored

--- app/app.js
+++ app/app.js
@@ -1,29 +1,27 @@
 require(['jquery',
 'backbone',
-'config',
-'jssdk/sdk/squid_api',
+'squid_api',
 'jssdk/sdk/widgets/squid_api_login_widget',
 'jssdk/sdk/widgets/squid_api_status',
 'widgets/hello'], 
-function($, Backbone, config, squid_api, LoginView, StatusView, ContentView) {
+function($, Backbone, squid_api, LoginView, StatusView, ContentView) {
+    
+    var loginView, statusView, contentView, config;
     
     /*
      * Declare the views 
      */
      
-    var loginView = new LoginView({
+    loginView = new LoginView({
         el : $('#login'),
-        model : squid_api.model.login,
-        loginUrl : config.loginUrl,
-        autoShow : true
+        autoShow : false
     });
     
-    var statusView = new StatusView({
-        el : $('#status'),
-        model : squid_api.model.status
+    statusView = new StatusView({
+        el : $('#status')
     });
     
-    var contentView = new ContentView({
+    contentView = new ContentView({
         el : $('#content')
     });
     
@@ -31,38 +29,26 @@ function($, Backbone, config, squid_api, LoginView, StatusView, ContentView) {
      * Controller part
      */
     
-    // handle logout
-    $('.logout').click(function(event){
-        event.preventDefault();
-        squid_api.model.login.logout();
-    });
-    
-    // handle session expiration
-    squid_api.model.status.on('change:error', function(model) {
-        var err = model.get("error").status;
-        if ((err == 401) || (err == 403)) {
-            squid_api.utils.clearLogin();
-        }
-    });
-
-    /*
-     * bootstrapping the APP
-     */
-    
     squid_api.model.login.on('change:login', function(model) {
         // performed when login is updated
         if (model.get("login")) {
             // login ok
             contentView.model.set({"message" : "Hello, you are logged with a user account on customer "+squid_api.customerId});
-            $('.sq-main').show();
+            
         } else {
             // login ko
-            $('.sq-main').hide();
+            contentView.model.set({"message" : "Please login"});
         }
+        $('#main').show();
     });
-     
-    squid_api.init({
-        apiURL: config.apiUrl,
-        clientId: config.clientId
-    });
+    
+    /*
+     * Start the App
+     */
+    config = {
+        "customerId" : null,
+        "clientId" : null,
+        "projectId" : null,
+    };
+    squid_api.init(config);
 });
